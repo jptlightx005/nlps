07bc6cb2394ac6472938bf6b13976004a7c170f7
jptlightx005 <jptlightx005@gmail.com>
2017-12-06 17:21:11 +0800

    Fixed authentication

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->enum('role', ['none', 'admin', 'officer']);
             $table->rememberToken();
             $table->timestamps();
         });
