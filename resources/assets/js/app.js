4b29c95b053f5d88f843a79489658c6bf031ee6c
pat-kun <jlteruel@rogomi.com>
2018-01-30 12:54:32 +0800

    Replaced google maps vue package

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,14 +9,25 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-import 'vue-googlemaps/dist/vue-googlemaps.css'
-import VueGoogleMaps from 'vue-googlemaps'
+// import 'vue-googlemaps/dist/vue-googlemaps.css'
+// import VueGoogleMaps from 'vue-googlemaps'
 
+// Vue.use(VueGoogleMaps, {
+//   load: {
+//     apiKey: 'AIzaSyD_XljDoRneX7A7Xgsqe17mq46pOgSp_lo',
+//     libraries: ['places'],
+//   },
+// })
+
+import * as VueGoogleMaps from 'vue2-google-maps'
 Vue.use(VueGoogleMaps, {
   load: {
-    apiKey: 'AIzaSyD_XljDoRneX7A7Xgsqe17mq46pOgSp_lo',
-    libraries: ['places'],
-  },
+    key: 'AIzaSyBvWE_sIwKbWkiuJQOf8gSk9qzpO96fhfY',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+  }
 })
 
 /**
--- resources/assets/js/components/MapComponent.vue
+++ resources/assets/js/components/MapComponent.vue
@@ -1,142 +1,90 @@
 <template>
-	<googlemaps-map
-	  :center.sync="center"
-	  :zoom.sync="zoom"
-	  :options="mapOptions"
-	  @idle="onIdle"
-	  @click="onMapClick">
-
-	  <!-- User Position -->
-	  <googlemaps-user-position
-	    @update:position="setUserPosition"
-	  />
-
-	  <googlemaps-marker
-	    v-for="marker of markers"
-	    :key="marker._id"
-	    :label="marker.label"
-	    :position="marker.position"
-	    @click="selectMarker(marker)"
-	  />
-	</googlemaps-map>
+    <gmap-map
+    :center="center"
+    :zoom="14"
+    >
+        <gmap-marker
+        :key="index"
+        v-for="(m, index) in markers"
+        :label="m.label"
+        :title="m.title"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="selectMarker(m)"
+        >
+            <gmap-info-window 
+                                :opened.sync="m.isClicked"
+                                @domready="infoWindowReady"
+                                @closeclick="infoWindowClose">
+                I can use <b>HTML</b> here and expressions too !<br>
+                I am not bound to any markers. I'm freeee.
+            </gmap-info-window>
+        </gmap-marker>
+    </gmap-map>
 </template>
 
 <script>
 export default {
+    methods: {
+        onIdle: function(){
 
-  name: 'MapComponent',
-  methods: {
-  	onIdle: function(){
+        },
+        onMapClick: function(){
 
-  	},
-  	onMapClick: function(){
+        },
+        setUserPosition: function(){
 
-  	},
-  	setUserPosition: function(){
+        },
+        selectMarker: function(marker){
+            // console.log(marker);
+            // console.log(this.$refs);
 
-  	},
-  	selectMarker: function(marker){
-  		var id = marker._id;
-  		var info = this.locations.filter(function(item){ return item.id == id;} ).pop();
-  		// console.log(this);
-  		// console.log(this.$children[0].$_map);
-  		var map = this.$children[0].$_map;
-  		console.log(map);
-  		// var map = ;
-  		//Location:
-        var location_name_group = $('<div>').attr({
-                class: "info-group"
-            })
-            .append($('<label>')
-                .addClass('info-label')
-                .text('Location: ')
-            ).append($('<span>')
-                .text(info.location_name)
-            )
+        },
+        infoWindowReady: function(){
+            // console.log("ready...")
+            // console.log(this);
+        },
+        infoWindowClose: function(infowindow){
+            // console.log("closed...")
+            // console.log(infowindow)
+        }
+    },
+    data () {
+        return {
+            center: {lat: 10.872001027667604, lng: 122.57285513977047},
+            markers: []
+        }
+    },
+    created(){
+        console.log(this.$children[0]);
+        axios.get('/locations').then(response => {
+            this.locations = response.data;
+            var list = [];
+            response.data.map(function(value, key) {
+                var position = {
+                    lat: parseFloat(value.lat),
+                    lng: parseFloat(value.long)
+                };
 
-        //Number of Crimes:
-        var crimes_group = $('<div>').attr({
-                class: "info-group"
-            })
-            .append($('<label>')
-                .addClass('info-label')
-                .text('Crimes: ')
-            ).append($('<span>')
-                .text(info.crimes.length)
-            )
-
-        //View Details
-        var details_button = $('<div>').attr({
-                class: "info-group"
-            })
-            .append($('<a>')
-                .attr({href: "#",
-                        class: "view-details",
-                        id: info.id})
-                .text('View Details')
-            )
-
+                var label = {
+                    color: 'black',
+                    fontFamily: 'Material Icons',
+                    text: value.crimes.length.toString(),
+                }
+                list.push({id: value.id, position: position, label: label, title: value.location_name});
+            });
         
-        //infowindow elements container
-        var location_info = $('<div>').attr({
-            class: "location-info"
-        })
-        .append(location_name_group)
-        .append(crimes_group)
-        .append(details_button);        
-
-        var infowindow = new google.maps.InfoWindow({
-          content: location_info.prop('outerHTML')
+            console.log(list);
+            this.markers = list;
         });
-
-        
-        infowindow.open(map, marker);
-
-        $('<span>').text(info.location_name).after(location_info)
-
-       
-        $('a#' + info.id).on('click', function(e){
-            didClickDetails(e, info.id);
-        });
-  	}
-  },
-  data () {
-    return {
-      center: { lat: 10.872001027667604, lng: 122.57285513977047 },
-      zoom: 14,
-      mapOptions: {},
-      markers: [],
-      locations: [],
-      map: null,
     }
-  },
-  created(){
-  	
-  	axios.get('/locations').then(response => {
-		this.locations = response.data;
-		var list = [];
-		response.data.map(function(value, key) {
-	     	var position = {
-	     					lat: parseFloat(value.lat),
-	     					lng: parseFloat(value.long)
-	     					};
-
-	     	var label = {
-	                    color: 'black',
-	                    fontFamily: 'Material Icons',
-	                    text: value.crimes.length.toString(),
-	                }
-	     	list.push({_id: value.id, position: position, label: label, title: value.location_name});
-	   	});
-	   	console.log(list);
-	   	this.markers = list;
-	});
-  }
 }
 </script>
 
 <style lang="css" scoped>
-	.vue-google-map{
-		height: 100%;
-	}
+    .vue-map-container{
+        height: 100%;
+        width: 100%;
+    }
 </style>
